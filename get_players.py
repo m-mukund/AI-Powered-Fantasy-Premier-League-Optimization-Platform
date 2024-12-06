775816422b44cbea0a1e203b2e8e9e895d8e43cb
M Mukund <mukundm8pp@gmail.com>
2024-12-05 23:44:07 -0700

    Python script to push player data to postgres

---
+++ get_players.py
@@ -0,0 +1,30 @@
+import psycopg2 
+import pandas as pd 
+from sqlalchemy import create_engine 
+
+
+conn_string = 'postgres://postgres:mukund@localhost/postgres'
+players_url="https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/refs/heads/master/data/2024-25/players_raw.csv"
+
+db = create_engine(conn_string) 
+conn = db.connect() 
+
+
+# our dataframe 
+players_df=pd.read_csv(players_url)
+
+
+players_df.to_sql('players', con=conn, if_exists='replace', 
+		index=False) 
+conn = psycopg2.connect(conn_string 
+						) 
+conn.autocommit = True
+cursor = conn.cursor() 
+
+sql1 = '''select * from players;'''
+cursor.execute(sql1) 
+for i in cursor.fetchall(): 
+	print(i) 
+
+# conn.commit() 
+conn.close() 
