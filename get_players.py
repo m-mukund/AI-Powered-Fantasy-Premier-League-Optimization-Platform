ff6ff6c099ae197b7abf954c8d781e2ab6d9d2ac
M Mukund <mukundm8pp@gmail.com>
2024-12-05 23:49:22 -0700

    Fixed module not found error

--- get_players.py
+++ get_players.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sqlalchemy import create_engine 
 
 
-conn_string = 'postgres://postgres:mukund@localhost/postgres'
+conn_string = 'postgresql://postgres:mukund@localhost/postgres'
 players_url="https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/refs/heads/master/data/2024-25/players_raw.csv"
 
 db = create_engine(conn_string) 
@@ -12,6 +12,7 @@ conn = db.connect()
 
 # our dataframe 
 players_df=pd.read_csv(players_url)
+players_df=players_df[['id', 'web_name']]
 
 
 players_df.to_sql('players', con=conn, if_exists='replace', 
@@ -21,7 +22,7 @@ conn = psycopg2.connect(conn_string
 conn.autocommit = True
 cursor = conn.cursor() 
 
-sql1 = '''select * from players;'''
+sql1 = '''select * from players LIMIT 10;'''
 cursor.execute(sql1) 
 for i in cursor.fetchall(): 
 	print(i) 
