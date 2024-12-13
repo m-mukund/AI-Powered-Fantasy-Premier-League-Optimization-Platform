830981b447a89ff980f35592f45e9f144068d02c
m-mukund <mukundm8pp@gmail.com>
2024-12-13 10:06:35 +0000

    Added Cronjob + Google Cloud Storage Functionality

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,14 @@ kubectl apply -f postgres/postgres-configmap.yaml
 kubectl apply -f postgres/postgres-pvc.yaml
 kubectl apply -f postgres/postgres-deployment.yaml
 kubectl apply -f postgres/postgres-service.yaml
+
+kubectl apply -f backend/backend-deployment.yaml
+kubectl apply -f backend/backend-service.yaml
+
+kubectl apply -f fpl-app/frontend-deployment.yaml
+kubectl apply -f fpl-app/frontend-service.yaml
+
+kubectl apply -f cronjob/pushpreds-cronjob.yaml
+
+kubectl apply -f redis/redis-deployment.yaml
+kubectl apply -f redis/redis-service.yaml
